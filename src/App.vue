e01da85fc6097601ffd94671212f59661e949758
Mariusz67 <putomariusz@gmail.com>
2022-05-28 14:08:33 +0200

    pierwszy

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div >
+    <h>Twój email to {{email}}</h>
+    {{email.length}}
+    <input type="text" v-model="email">
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+export default{
+  data(){
+    return {
+      email:'',
+    };
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
